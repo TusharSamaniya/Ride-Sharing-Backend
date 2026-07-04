9614abe253b9066e8222081cd09f3a13517ef4b2
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-07-04 16:02:46 +0530

    configuring the jwt

---
+++ src/main/java/com/rideshare/dto/auth/RegisterRequest.java
@@ -0,0 +1,43 @@
+package com.rideshare.dto.auth;
+
+import com.rideshare.entity.UserRole;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+	
+	@NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone must be 10 digits")
+    private String phoneNumber;
+
+    @NotNull(message = "Role is required")
+    private UserRole role; 
+
+}
