ca457049b4f952575bb8bccebfeb37058cab144b
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-07-11 14:57:17 +0530

    deploying to render

--- src/test/java/com/rideshare/RideSharingBackendApplicationTests.java
+++ src/test/java/com/rideshare/RideSharingBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.rideshare;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class RideSharingBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }
